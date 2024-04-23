8d375d504957c827eafab17d1e6082fe8e45d9b9
SaraVieira <hey@iamsaravieira.com>
2024-04-23 11:20:21 +0200

    camera fix

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -6,7 +6,7 @@ import { EffectComposer, Pixelation } from "@react-three/postprocessing";
 export const Scene = ({ cover }) => {
   return (
     <>
-      <Stage environment={"forest"} adjustCamera={1.35} shadows={false}>
+      <Stage environment={"forest"} adjustCamera={1.44} shadows={false}>
         <Case cover={cover} />
       </Stage>
       <ContactShadows position={[0, -0.7, 0]} opacity={0.5} />
@@ -16,7 +16,7 @@ export const Scene = ({ cover }) => {
         position={[0, 2, -1]}
         rotation={[-Math.PI / 2, 0, 0]}
       />
-      <FPSLimiter fps={10} />
+      <FPSLimiter fps={30} />
       <OrbitControls />
       <EffectComposer>
         <Pixelation granularity={5} />
